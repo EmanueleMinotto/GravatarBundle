f9b1d6f8d97f1e64def0d3e661a882ddc0837d41
Travis <travis@travis-ci.org>
2015-10-11 09:41:17 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","EmanueleMinotto\\GravatarBundle\\DependencyInjection\\Configuration"],["c","EmanueleMinotto\\GravatarBundle\\DependencyInjection\\GravatarExtension"],["c","EmanueleMinotto\\GravatarBundle\\GravatarBundle"]];
